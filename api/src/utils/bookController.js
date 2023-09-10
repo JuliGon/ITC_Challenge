ef840cd57e837e6c2eb54f345689fbad41664a4b
juligon <julianagon73@gmail.com>
2023-09-10 13:57:57 -0300

    fixing bugs

--- api/src/utils/bookController.js
+++ api/src/utils/bookController.js
@@ -1,58 +1,95 @@
-const { Op } = require("sequelize");
 const { Book } = require("../db");
+const { Op } = require("sequelize");
 
 // Función para obtener todos los libros y aplicar filtros de nombre y descripción
 const getAllBooks = async (req, res, next) => {
-  const { name, description } = req.query;
-  let whereClause = {};
+	const { name, description } = req.query;
+	let whereClause = {};
 
-  if (name) {
-    whereClause.name = {
-      [Op.iLike]: `%${name}%`,
-    };
-  }
+	if (name) {
+		whereClause.name = {
+			[Op.iLike]: `%${name}%`,
+		};
+	}
 
-  if (description) {
-    whereClause.description = {
-      [Op.iLike]: `%${description}%`,
-    };
-  }
+	if (description) {
+		whereClause.description = {
+			[Op.iLike]: `%${description}%`,
+		};
+	}
 
-  try {
-    const books = await Book.findAll({
-      where: whereClause,
-    });
+	try {
+		const books = await Book.findAll({
+			where: whereClause,
+		});
 
-    if (books.length === 0) {
-      const error = new Error("Books not found");
-      error.status = 404;
-      throw error;
-    }
+		if (books.length === 0) {
+			const error = new Error("Books not found");
+			error.status = 404;
+			throw error;
+		}
 
-    res.json(books);
-  } catch (error) {
-    next(error);
-  }
-}
+		res.json(books);
+	} catch (error) {
+		next(error);
+	}
+};
 
 // Función para buscar un libro por su ID
 const getBookById = async (req, res, next) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  try {
-    const book = await Book.findByPk(id);
+	try {
+		const book = await Book.findByPk(id);
 
-    if (!book) {
-      const error = new Error("Book not found");
-      error.status = 404;
-      throw error;
-    }
+		if (!book) {
+			const error = new Error("Book not found");
+			error.status = 404;
+			throw error;
+		}
 
-    res.json(book);
-  } catch (error) {
-    next(error);
-  }
-}
+		res.json(book);
+	} catch (error) {
+		next(error);
+	}
+};
+
+// Función para crear un nuevo libro
+const createIndividualBook = async ({
+	name,
+	author,
+	description,
+	image_url,
+	price,
+	editorialId,
+}) => {
+	const existingBook = await Book.findOne({
+		where: { name: { [Op.iLike]: `%${name}%` } },
+	});
+
+	if (existingBook) {
+		const error = new Error("The book already exists");
+		error.status = 404;
+		throw error;
+	}
+
+	const newBook = await Book.create({
+		name: name,
+		author: author,
+		description: description,
+		image_url: image_url,
+		price: price,
+		editorialId: editorialId,
+	});
+
+	return newBook;
+};
+
+// Función para crear libros a partir de un array
+const bulkCreateBooks = async (booksData) => {
+	const books = await Book.bulkCreate(booksData);
+	return books;
+};
 
 // Función para crear un nuevo libro o libros
 const createBook = async (req, res, next) => {
@@ -60,41 +97,22 @@ const createBook = async (req, res, next) => {
 		const requestData = req.body;
 
 		if (Array.isArray(requestData)) {
-			// Si requestData es un array, crea libros en lote
-			const createdBooks = await Book.bulkCreate(requestData);
-			res.json(createdBooks);
+			const books = await bulkCreateBooks(requestData);
+			res.json(books);
 		} else if (typeof requestData === "object") {
-			// Si requestData es un objeto, crea un libro individual
-			const { name, author, description, image_url, price, editorialId } = requestData;
-			
-			const existingBook = await Book.findOne({
-				where: { name: { [Op.iLike]: `%${name}%` } },
-			});
-
-			if (existingBook) {
-				const error = new Error("The book already exists");
-				error.status = 404;
-				throw error;
-			}
-
-			const newBook = await Book.create({
-				name: name,
-				author: author,
-				description: description,
-				image_url: image_url,
-				price: price,
-				editorialId: editorialId,
-			});
+			const newBook = await createIndividualBook(requestData);
 			res.status(201).json(newBook);
 		} else {
-			const error = new Error("Invalid data format.");
-			error.status = 400; // Código de estado HTTP 400 (Bad Request)
+			const error = new Error(
+				"Invalid data format. Request data must be an object or an array."
+			);
+			error.status = 400;
 			throw error;
 		}
 	} catch (error) {
 		next(error);
 	}
-}
+};
 
 // Función para eliminar un libro por su ID
 const deleteBook = async (req, res, next) => {
@@ -112,7 +130,7 @@ const deleteBook = async (req, res, next) => {
 	} catch (error) {
 		next(error);
 	}
-}
+};
 
 // Función para actualizar un libro por su ID
 const updateBook = async (req, res, next) => {
@@ -134,7 +152,7 @@ const updateBook = async (req, res, next) => {
 			description,
 			image_url,
 			price,
-			editorialId
+			editorialId,
 		});
 
 		const updatedBook = await Book.findOne({
@@ -145,7 +163,7 @@ const updateBook = async (req, res, next) => {
 	} catch (error) {
 		next(error);
 	}
-}
+};
 
 module.exports = {
 	getAllBooks,
@@ -154,4 +172,3 @@ module.exports = {
 	deleteBook,
 	updateBook,
 };
-
--- api/src/utils/editorialController.js
+++ api/src/utils/editorialController.js
@@ -18,11 +18,13 @@ const getAllEditorials = async (req, res, next) => {
 	}
 };
 
-// Función para obtener una editorial por su ID (con ID como parámetro de consulta)
+// Función para obtener una editorial por su ID
 const getEditorialById = async (req, res, next) => {
 	const { id } = req.params;
+
 	try {
 		const editorial = await Editorial.findByPk(id);
+
 		if (!editorial) {
 			const error = new Error("Editorial not found");
 			error.status = 404;
@@ -36,24 +38,23 @@ const getEditorialById = async (req, res, next) => {
 
 // Función para crear una nueva editorial
 const createIndividualEditorial = async ({ name, logo_url }) => {
-  const existingEditorial = await Editorial.findOne({
-    where: { name: { [Op.iLike]: `%${name}%` } },
-  });
+	const existingEditorial = await Editorial.findOne({
+		where: { name: { [Op.iLike]: `%${name}%` } },
+	});
 
-  if (existingEditorial) {
-    const error = new Error("The editorial already exists");
-    error.status = 404;
-    throw error;
-  }
+	if (existingEditorial) {
+		const error = new Error("The editorial already exists");
+		error.status = 404;
+		throw error;
+	}
 
-  const newEditorial = await Editorial.create({
-    name: name,
-    logo_url: logo_url,
-  });
+	const newEditorial = await Editorial.create({
+		name: name,
+		logo_url: logo_url,
+	});
 
-  return newEditorial;
+	return newEditorial;
 };
-
 
 // Función para crear editoriales a partir de un array
 const bulkCreateEditorials = async (editorialsData) => {
@@ -63,29 +64,27 @@ const bulkCreateEditorials = async (editorialsData) => {
 
 // Función para crear una nueva editorial o editoriales
 const createEditorial = async (req, res, next) => {
-  try {
-    const requestData = req.body;
+	try {
+		const requestData = req.body;
 
-    if (Array.isArray(requestData)) {
-      const editorials = await bulkCreateEditorials(requestData);
-      res.json(editorials);
-    } else if (typeof requestData === "object") {
-      const newEditorial = await createIndividualEditorial(requestData); // Pasa el objeto completo
-      res.status(201).json(newEditorial);
-    } else {
-      const error = new Error(
-        "Invalid data format. Request data must be an object or an array."
-      );
-      error.status = 400;
-      throw error;
-    }
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
+		if (Array.isArray(requestData)) {
+			const editorials = await bulkCreateEditorials(requestData);
+			res.json(editorials);
+		} else if (typeof requestData === "object") {
+			const newEditorial = await createIndividualEditorial(requestData); 
+			res.status(201).json(newEditorial);
+		} else {
+			const error = new Error(
+				"Invalid data format. Request data must be an object or an array."
+			);
+			error.status = 400;
+			throw error;
+		}
+	} catch (error) {
+		console.error(error);
+		next(error);
+	}
 };
-
-
 
 module.exports = {
 	getAllEditorials,
