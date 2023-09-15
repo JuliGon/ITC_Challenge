3770d7e475ad9b6025446e42289fbd894442979b
juligon <julianagon73@gmail.com>
2023-09-15 20:01:54 -0300

    fixing bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/home/Home";
-import Detail from "./pages/detail/Detail";
-import Admin from "./pages/admin/Admin";
-import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home.jsx";
+import Detail from "./pages/detail/Detail.jsx";
+import Admin from "./pages/admin/Admin.jsx";
+import Navbar from "./components/navbar/Navbar.jsx";
 
 function App() {
   const [books, setBooks] = useState([]);
