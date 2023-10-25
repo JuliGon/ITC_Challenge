12af04cceb6894c0672cde4c0c16cf02a42d69e4
juligon <julianagon73@gmail.com>
2023-10-25 18:56:37 -0300

    footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,31 +5,28 @@ import Home from "./pages/home/Home.jsx";
 import Detail from "./pages/detail/Detail.jsx";
 import Admin from "./pages/admin/Admin.jsx";
 import Navbar from "./components/navbar/Navbar.jsx";
+import Footer from "./components/footer/Footer.jsx";
 
 function App() {
-  const [books, setBooks] = useState([]);
+	const [books, setBooks] = useState([]);
 
-  const handleSearchResults = (searchResults) => {
-    setBooks(searchResults);
-  };
+	const handleSearchResults = (searchResults) => {
+		setBooks(searchResults);
+	};
 
-  return (
-    <Router>
-      <div>
-        <Navbar onSearch={handleSearchResults} />
-        <Routes>
-          <Route exact path="/books/:id" element={<Detail />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route
-            exact
-            path="/"
-            element={<Home books={books} />}
-          />
-        </Routes>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div>
+				<Navbar onSearch={handleSearchResults} />
+				<Routes>
+					<Route exact path="/books/:id" element={<Detail />} />
+					<Route path="/admin" element={<Admin />} />
+					<Route exact path="/" element={<Home books={books} />} />
+				</Routes>
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
-
